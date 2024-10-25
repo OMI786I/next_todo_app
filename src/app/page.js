8401b227bca62cbbede6cc3ea4cecf11cee9065b
OMI786I <sohrawardy1998@gmail.com>
2024-10-26 01:47:35 +0600

    added registration page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,20 @@
-import Image from "next/image";
+import Link from "next/link";
+import Login from "./login/page";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-blue-500 to-indigo-700 text-white">
+      <div className="p-8 text-center bg-blue-800 rounded-lg shadow-lg max-w-lg">
+        <h1 className="text-6xl font-bold mb-4">Welcome!</h1>
+        <p className="text-xl font-light mb-8">
+          A short and fun todo app made with next js
+        </p>
+        <Link href={"/login"}>
+          <button className="px-8 py-4 text-lg font-semibold text-indigo-700 bg-white rounded-full shadow-md hover:bg-indigo-100 transform transition duration-300 ease-in-out hover:scale-105 focus:outline-none">
+            Get Started
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
