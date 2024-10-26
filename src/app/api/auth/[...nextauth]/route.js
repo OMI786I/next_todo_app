7cd9b2e6c272c8fe873967efc473df49e665872e
OMI786I <sohrawardy1998@gmail.com>
2024-10-26 11:54:13 +0600

    fixed error

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          required: true,
+          placeholder: "your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          required: true,
+          placeholder: "your email",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+        return true;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,13 +19,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <SessionProvider>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </SessionProvider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
